33f6a6633223f007b6ff5eb7ee58d1b4db4f9dc0
Arctangent1759 <alex.p.chu@gmail.com>
2013-05-28 21:35:11 -0700

    socket.io

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var http = require('http');
 var url = require('url');
+var io = require('socket.io');
 
 function start(route,handle){
   console.log("Server starting...");
 
-  http.createServer(function(request,response){
+  var server = http.createServer(function(request,response){
 	var pathname = url.parse(request.url).pathname;
 	route(handle,pathname,response);
-  }).listen(1759);
-  console.log("Server started. Press Ctrl-c to stop.");
+  }).listen(1759,function(){
+	console.log("Server started.");
+	console.log("Listening at http://localhost:1759");
+	console.log("Press Ctrl-c to stop.");
+  });
+  io.listen(server).on('connection',function(socket){
+  });
 }
 exports.start=start;
