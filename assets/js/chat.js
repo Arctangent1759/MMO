944da93bf1621cc11e4d8798f3bb81c75ce6d820
Arctangent1759 <alex.p.chu@gmail.com>
2013-06-11 01:07:37 -0700

    housekeeping

---
+++ assets/js/chat.js
@@ -0,0 +1,20 @@
+//  Note:
+//  This code will crash and burn, unless the name 'socket' is already defined in the namespace.
+
+function sendChat(message,channel){
+  var msg={
+	sessionKey:sessionKey,
+	channel:channel,
+	message:message,
+  };
+  socket.emit('chat',msg);
+}
+
+function recieveChat(data){
+  //TODO: Actually do stuff with UI.
+  console.log('@'+data.timestamp+'  ---  '+data.sender+' >> '+data.channel+': ' + data.message);
+}
+
+function setupChat(){
+  socket.on('chat',recieveChat);
+}
