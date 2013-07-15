94b146fb37893b13004221abc3c26aa7c7436799
William Wu <williamwu.2k12@gmail.com>
2013-07-14 23:19:57 -0700

    did graphics things and other things, but not done yet

--- assets/js/dataStructure.js
+++ assets/js/dataStructure.js
@@ -1,8 +1,32 @@
+// JSFIDDLE LINK: http://jsfiddle.net/QDSpG/8/
+
+
 // CIRCLE EXAMPLE OF CORNERS
+function distance(integer1, integer2) // this function here because needed for something referenced
+{
+    return Math.sqrt((integer1 * integer1) + (integer2 * integer2));
+}
+
+
 function addCorner(cornerArray, xPosition, yPosition)
 {
     var index = cornerArray.length
     cornerArray[index] = {"xPosition": xPosition, "yPosition": yPosition};
+}
+
+
+function mergeCorners(cornerArray1, cornerArray2)
+{
+    var returnArray = new Array();
+    for (index = 0; index < cornerArray1.length; index = index + 1)
+    {
+        returnArray.push(cornerArray1[index]);
+    }
+    for (index = 0; index < cornerArray2.length; index = index + 1)
+    {
+        returnArray.push(cornerArray2[index]);
+    }
+    return returnArray;
 }
 
 
@@ -67,15 +91,111 @@ function createCircle(radius)
 }
 
 
+function createLine(pointA, pointB) // to be done
+{
+    var slope = (pointB.yPosition - pointA.yPosition) / (pointB.xPosition - pointA.xPosition);   
+}
+
+
+function createSquare(radius)
+{
+    var cornerArray = new Array();
+    var xPosition = radius;
+    var yPosition = radius;
+    while (yPosition > -(radius))
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        yPosition = yPosition - 1;
+    }
+    while (xPosition > -(radius))
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        xPosition = xPosition - 1;
+    }
+    while (yPosition < radius)
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        yPosition = yPosition + 1;
+    }
+    while (xPosition < radius)
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        xPosition = xPosition + 1;
+    }
+    return cornerArray;
+}
+
+
+function createTriangle(radius)
+{
+    var cornerArray = new Array();
+    var xPosition = 0;
+    var yPosition = radius;
+    var index = 0;
+    while (xPosition < ((Math.sqrt(3) / 2) * radius))
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        yPosition = yPosition - 1;
+        if (index == 2)
+        {
+            xPosition = xPosition + 1;
+            index = -1;
+        }
+        index = index + 1;
+    }
+    while (xPosition > (-(Math.sqrt(3) / 2) * radius))
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        xPosition = xPosition - 1;
+    }
+    index = 0;
+    while (xPosition < 0)
+    {
+        cornerArray.push({"xPosition": xPosition, "yPosition": yPosition});
+        yPosition = yPosition + 1;
+        if (index == 2)
+        {
+            xPosition = xPosition + 1;
+            index = -1;
+        }
+        index = index + 1;
+    }
+    return cornerArray;
+}
+
+
 function shiftCorners(cornerArray, distanceX, distanceY) // shift from origin at 0, 0 to top-left corner, whilst inverting the y axis
 {
     var shiftedArray = cornerArray;
     for (index = cornerArray.length; index > 0; index = index - 1)
     {
         shiftedArray[index - 1].xPosition = shiftedArray[index - 1].xPosition + distanceX;
-        shiftedArray[index - 1].yPosition = -(shiftedArray[index - 1].yPosition - distanceY);
+        shiftedArray[index - 1].yPosition = -(shiftedArray[index - 1].yPosition - (distanceY + 1)); // the plus one is because coordinates with 0 will not show
     }
     return shiftedArray;
+}
+
+
+function shiftArray(cornerArray, distanceX, distanceY) // consistent with the common 'positive is up' screen x and y orientation
+{
+    var shiftedArray = cornerArray;
+    for (index = 0; index < cornerArray.length; index = index + 1)
+    {
+        shiftedArray[index].xPosition = shiftedArray[index].xPosition + distanceX;
+        shiftedArray[index].yPosition = shiftedArray[index].yPosition + distanceY;
+    }
+    return shiftedArray;
+}
+
+
+function invertArray(cornerArray)
+{
+    var invertedArray = cornerArray;
+    for (index = 0; index < cornerArray.length; index = index + 1)
+    {
+        invertedArray[index].yPosition = -invertedArray[index].yPosition;
+    }
+    return invertedArray;
 }
 
 
@@ -89,19 +209,69 @@ function drawShape(cornerArray, imageData, red, green, blue, alpha)
         imageData.data[4 * pixelIndex + 2] = blue;
         imageData.data[4 * pixelIndex + 3] = alpha;
     }
+//    for (index = 0; index < imageData.data.length; index = index + 4) // to get rid of whitespace in the imageData, unfortunately doesn't work yet
+//    {
+//        var color = imageData.data[index]
+//        if (imageData.data[index + 0] == color && imageData.data[index + 1] == color && imageData.data[index + 2] == color)
+//        {
+//            imageData.data[index + 3] = 0;
+//        }
+//    }
+//    for (index = 0; index < imageData.data.length; index = index + 1)
+//    {
+//        if (imageData.data[index] == 255 && ((index + 1) % 4 == 0) && imageData.data[index - 1] == 0 && imageData.data[index - 2] == 0 && imageData.data[index - 3] == 0)
+//        {
+//            imageData.data[index] = 0;
+//        }
+//    }
 }
 
 
-// to actually display
-var Circle1 = new createCircle(50);
-var shiftedCircle1 = shiftCorners(Circle1, 50, 50);
+
+////////////////////////
+//  simple collision  //
+////////////////////////
+
+var circle = new createPlayerObject("Circle", undefined, 0, 0, createCircle(50), 10, 10, 10);
+var square = new createPlayerObject("Square", undefined, 0, 0, createSquare(69), 10, 10, 10);
+networkObject1 = {"xPosition": 0, "yPosition": 0};
+networkObject2 = {"xPosition": 0, "yPosition": 100};
+console.log(circle.collision(networkObject1, networkObject2, square));
+
+// or what we could do is have a separate js file made for physics, and put all the collision stuff in there, since a lot of the functions are repeated
+// create an object from object constructor function physics, such that the prototype is: physics.collision(networkObject1, networkObject2, localObject1, localObject2)
+
+
+
+
+
+//////////////////////
+//  simple drawing  //
+//////////////////////
+
+var circle = new createCircle(50);
+var shiftedCircle = shiftCorners(circle, 50, 50);
+var square = new createSquare(50);
+var shiftedSquare = shiftCorners(square, 50, 50);
+var triangle = new createTriangle(50);
+var shiftedTriangle = shiftCorners(triangle, 44, 50);
 
 var canvas = document.getElementById("canvasID");
 var ctx = canvas.getContext("2d");
-var imgData = ctx.createImageData(100, 100);
 
-drawShape(shiftedCircle1, imgData, 255, 0, 0, 255);
-ctx.putImageData(imgData, 10, 10);
+var circleData = ctx.createImageData(150, 150);
+var squareData = ctx.createImageData(150, 150);
+var triangleData = ctx.createImageData(150, 150);
+
+drawShape(shiftedCircle, circleData, 255, 0, 0, 255);
+drawShape(shiftedSquare, squareData, 255, 0, 0, 255);
+drawShape(shiftedTriangle, triangleData, 255, 0, 0, 255);
+ctx.putImageData(circleData, 0, 0);
+ctx.putImageData(squareData, 150, 150);
+ctx.putImageData(triangleData, 300, 300);
+
+// instead of doing the repetitive code, a for loop on an array of objects will be done instead (in the actual game)
+// triangle has bad angles, need to figure out how to make it equilateral
 
 
 
@@ -111,9 +281,165 @@ ctx.putImageData(imgData, 10, 10);
 
 
 
+// where html == <canvas id="canvasID" width="500" height="500"></canvas>
 
-// FOR LOCAL MAPOBJECT
-function createMapObject(objectName, spriteLink, xOrigin, yOrigin, corners)
+// object creation
+var circle = new createPlayerObject("circle", undefined, 0, 0, createCircle(50), 10, 10, 10);
+var square = new createPlayerObject("square", undefined, 0, 0, createSquare(50), 10, 10, 10); // stationary object in this test
+circle.corners = shiftCorners(circle.corners, 50, 50)
+square.corners = shiftCorners(square.corners, 50, 50)
+
+// location setting
+var tempX = 0 - circle.radius; // this block must be after the declaration of the playerObject, since it references the radius of a circle that hasn't been declared yet
+var tempY = 0 - circle.radius;
+var fixedX = 250 - square.radius;
+var fixedY = 250 - square.radius;
+var changeX = 0;
+var changeY = 0;
+
+// canvas creation
+var canvas = document.getElementById("canvasID");
+var ctx = canvas.getContext("2d");
+var canvasData = ctx.createImageData(canvas.width, canvas.height);
+drawShape(shiftCorners(circle.corners, changeX, changeY), canvasData, 255, 0, 0, 255);
+ctx.putImageData(canvasData, 0, 0);
+
+// var canvasData = ctx.createImageData(canvas.width, canvas.height);
+// drawShape(shiftCorners(circle.corners, tempX, tempY), canvasData, 255, 0, 0, 255);
+// drawShape(shiftCorners(square.corners, fixedX, fixedY), canvasData, 255, 0, 0, 255);
+// ctx.putImageData(canvasData, 0, 0);
+
+// var circleData = ctx.createImageData(circleRadius, circleRadius);
+// var squareData = ctx.createImageData(squareRadius, squareRadius);
+// drawShape(circle.corners, circleData, 255, 0, 0, 255);
+// drawShape(square.corners, squareData, 255, 0, 0, 255);
+// ctx.putImageData(circleData, tempX, tempY);
+// ctx.putImageData(squareData, fixedX, fixedY)
+
+var up, down, left, right;
+document.onkeydown = function(keyEvent)
+{    
+    if (keyEvent.which == 87)
+    {
+        up = true;
+    }
+    if (keyEvent.which == 65)
+    {
+        left = true;
+    }
+    if (keyEvent.which == 83)
+    {
+        down = true;
+    }
+    if (keyEvent.which == 68)
+    {
+        right = true;
+    }
+}
+document.onkeyup = function(keyEvent)
+{
+    if (keyEvent.which == 87)
+    {
+        up = false;
+    }
+    if (keyEvent.which == 65)
+    {
+        left = false;
+    }
+    if (keyEvent.which == 83)
+    {
+        down = false;
+    }
+    if (keyEvent.which == 68)
+    {
+        right = false;
+    }
+}
+var prevX = new Array();
+var prevY = new Array();
+var prevC = new Array();
+
+function update()
+{
+    if (circle.collision({"xPosition": tempX, "yPosition": tempY}, {"xPosition": fixedX, "yPosition": fixedY}, square) == false)
+    {
+        if (up == true)
+        {
+            tempY = tempY - 1;
+        }
+        if (down == true)
+        {
+            tempY = tempY + 1;
+        }
+        if (left == true)
+        {
+            tempX = tempX - 1;
+        }
+        if (right == true)
+        {
+            tempX = tempX + 1;
+        }
+        prevC.push(false);
+    }
+    else
+    {
+        prevC.push(true);
+        var index = prevC.length - 1;
+        while (prevC[index] == true)
+        {
+            index = index - 1;
+        }
+        tempX = prevX[index]; // this is great because you have a history of all the places this object has been, which means you can draw on a minimap too
+        tempY = prevY[index];
+    }
+}
+
+var animate = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    null;
+
+var mainloop = function()
+{
+    prevX.push(tempX);
+    prevY.push(tempY);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the canvas
+    update();
+    for (index = 0; index < canvasData.data.length; index = index + 1)
+    {
+        canvasData.data[index] = 0;
+    }
+    drawShape(shiftCorners(circle.corners, tempX, tempY), canvasData, 255, 0, 0, 255)
+    drawShape(shiftCorners(square.corners, tempX, tempY), canvasData, 255, 0, 0, 255)
+    ctx.putImageData(canvasData, 0, 0);
+//    ctx.putImageData(circleData, tempX, tempY); // offset by 50 by 50 to move item by origin rather than by upper left corner
+//    ctx.putImageData(squareData, fixedX, fixedY)
+}
+
+var recursiveAnimate = function()
+{
+    mainloop();
+    animate(recursiveAnimate);
+}
+if (animate != null)
+{
+    animate(recursiveAnimate);
+}
+else
+{
+    setInterval(mainloop, 16);
+}
+
+
+
+
+
+
+
+// FOR LOCAL PLAYEROBJECT
+function createPlayerObject(objectName, spriteLink, xOrigin, yOrigin, corners, acceleration, moveSpeed, turnSpeed)
 {
     this.objectName = objectName;
     this.spriteLink = spriteLink;
@@ -146,7 +472,70 @@ function createMapObject(objectName, spriteLink, xOrigin, yOrigin, corners)
         {
             for (index2 = 0; index2 < this.corners.length; index2 = index2 + 1)
             {
-                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition = networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition = networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
+                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition == networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition == networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    this.acceleration = acceleration;
+    this.moveSpeed = moveSpeed;
+    this.turnSpeed = turnSpeed;
+
+//  function calls
+    this.setRadius();
+}
+
+
+
+
+
+// FOR LOCAL MAPOBJECT
+function createMapObject(objectName, spriteLink, xOrigin, yOrigin, corners)
+//////////////////////////////
+//                          //
+//  Inputs (Ordered)        //
+//  objectName, spriteLink  //
+//  xOrigin, yOrigin        //
+//  corner                  //
+//                          //
+//////////////////////////////
+{
+    this.objectName = objectName;
+    this.spriteLink = spriteLink; // reconsider changing this to the natural src extension, since this apparently goes well with it
+    this.xOrigin = xOrigin;
+    this.yOrigin = yOrigin;
+
+    this.corners = corners;
+    this.radius = 0;
+    this.setRadius = function() // how to get function to run only once and put return value in variable?; update 06-25-13, simply call the function from within
+    {
+        var longestRadius = 0;
+        for (index0 = 0; index0 < this.corners.length; index0 = index0 + 1)
+        {
+            if (longestRadius < distance(this.corners[index0].xPosition, this.corners[index0].yPosition))
+            {
+                longestRadius = distance(this.corners[index0].xPosition, this.corners[index0].yPosition);
+            }
+        }
+        this.radius = longestRadius;
+    }
+    this.collision = function(networkObject1, networkObject2, localObject2) // localObject1 is this
+    {
+        var radiusThis = this.radius;
+        var radiusThat = localObject2.radius;
+        if (radiusThis + radiusThat < distance(networkObject2.xPosition - networkObject1.xPosition, networkObject2.yPosition - networkObject1.yPosition)) // checking if the objects are even close enough to consider collision
+        {
+            return false;
+        }
+        for (index1 = 0; index1 < localObject2.corners.length; index1 = index1 + 1)
+        {
+            for (index2 = 0; index2 < this.corners.length; index2 = index2 + 1)
+            {
+                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition == networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition == networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
                 {
                     return true;
                 }
@@ -218,7 +607,7 @@ function createMapObject(objectName, spriteLink, xOrigin, yOrigin, corners)
         }
         if ((projectileAngle > intersectionAngle) && (projectileAngle > (intersectionAngle + Math.PI))) // filter circle to appropriate arc, also, this is assuming projectileObject.angle is in radians from -Math.PI to Math.PI
         {
-            this.corners.splice(intersection[0], 1 + intersection[1] - intersection[0];
+            this.corners.splice(intersection[0], 1 + intersection[1] - intersection[0]);
         }
         else
         {
@@ -232,6 +621,9 @@ function createMapObject(objectName, spriteLink, xOrigin, yOrigin, corners)
             this.cornerChange("add", cornerAdjusted.xPosition, cornerAdjusted.yPosition);
         }
     }
+
+//  function calls
+    this.setRadius();
 }
 
 
@@ -288,7 +680,7 @@ function createProjectileObject(objectName, spriteLink, xOrigin, yOrigin, mass, 
         {
             for (index2 = 0; index2 < this.corners.length; index2 = index2 + 1)
             {
-                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition = networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition = networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
+                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition == networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition == networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
                 {
                     return true;
                 }
@@ -362,7 +754,7 @@ function createWeaponObject(objectName, spriteLink, xOrigin, yOrigin, projectile
         {
             for (index2 = 0; index2 < this.corners.length; index2 = index2 + 1)
             {
-                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition = networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition = networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
+                if ((networkObject2.xPosition + localObject2.corners[index1].xPosition == networkObject1.xPosition + this.corners[index2].xPosition) && (networkObject2.yPosition + localObject2.corners[index1].yPosition == networkObject1.yPosition + this.corners[index2].yPosition)) // check if the corners of network objects are equal to each other
                 {
                     return true;
                 }
